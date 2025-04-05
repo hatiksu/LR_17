8027227673ff5bb2990afa3fff5d0c2c9ad10fb0
Tosha Hatiksu <hatiksuofficial@gmail.com>
2025-04-05 09:32:24 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,58 @@
+﻿  type
+  PNode = ^TNode;
+  TNode = record
+    Data: Integer;
+    Next: PNode;
+  end;
+
+var
+  InputFile, OutputFile: Text;
+  StackTop, TempNode: PNode;
+  Number: Integer;
+
+procedure Push(var Top: PNode; Value: Integer);
+var
+  NewNode: PNode;
+begin
+  New(NewNode);
+  NewNode^.Data := Value;
+  NewNode^.Next := Top;
+  Top := NewNode;
+end;
+
+function Pop(var Top: PNode): Integer;
+begin
+  if Top = nil then
+  begin
+    Writeln('Ошибка: стек пуст.');
+    Halt(1);
+  end;
+  Pop := Top^.Data;
+  TempNode := Top;
+  Top := Top^.Next;
+  Dispose(TempNode);
+end;
+
+begin
+  Assign(InputFile, 'input1.txt');
+  Assign(OutputFile, 'output1.txt');
+  Reset(InputFile);
+  Rewrite(OutputFile);
+
+  StackTop := nil;
+
+  while not Eof(InputFile) do
+  begin
+    Readln(InputFile, Number);
+    Push(StackTop, Number);
+  end;
+
+  while StackTop <> nil do
+  begin
+    Number := Pop(StackTop);
+    Writeln(OutputFile, Number);
+  end;
+
+  Close(InputFile);
+  Close(OutputFile);
+end.
